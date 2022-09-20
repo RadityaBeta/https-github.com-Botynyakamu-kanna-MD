7bb37fcddac720f624cf71efd667bbd6a0b770ae
RadityaBeta <111179244+RadityaBeta@users.noreply.github.com>
2022-09-20 19:20:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,8 @@ Kannachann
 The.sad.Boy01
 Rasel comel
 Xtreshe (Beban)
-Dll
+RadityaX
+Choriul huda
 */
 
 import { watchFile, unwatchFile } from 'fs'
@@ -41,22 +42,22 @@ let wibh = moment.tz('Asia/Jakarta').format('HH')
     
 
 /*============== SOCIAL ==============*/
-global.sig = 'https://instagram.com/zalfapontianak'
-global.sgh = 'https://github.com/Botynyakamu'
-global.sgc = 'https://chat.whatsapp.com/I8Q4oJVw8buHhIgMH5iVAv'
-global.sdc = 'https://wa.me/message/LSVGQQPG6FUQF1'
-global.snh = 'https://youtube.com/channel/UCeUvY_fIVSAaQ1n5SAylI-A'
+global.sig = 'https://instagram.com/raditya_fulca'
+global.sgh = 'https://github.com/RadityaBeta'
+global.sgc = 'https://chat.whatsapp.com/Eg2nPVdgcas6Tu2KakxMdw'
+global.sdc = 'wa.me//6281227259557'
+global.snh = 'https://youtube.com'
 
 /*============== PAYMENT ==============*/
-global.pdana = '085828764046'
-global.povo = '085828764046'
-global.pgopay = '085828764046'
+global.pdana = '081227259557'
+global.povo = '081227259557'
+global.pgopay = '081227259557'
 
 /*============== NOMOR ==============*/
-global.nomorbot = '6285787476829'
-global.nomorown = '6285828377610'
-global.namebot = '⳹ ❋ཻུ۪۪⸙Zifabotz-MD⳹ ❋ཻུ۪۪'
-global.nameown = 'ᴹᴿ᭄ ZifaOffcོ_1ঔৣ꧂'
+global.nomorbot = '6282141034454'
+global.nomorown = '6281227259557'
+global.namebot = 'Radityaa-MD'
+global.nameown = 'ᴹᴿ RadityaaOFC'
 
 
 /*============== STAFF ==============*/
@@ -109,13 +110,13 @@ global.APIKeys = { // APIKey Here
 
 
 /*============== WATERMARK ==============*/
-global.wm = '⳹ ❋ཻུ۪۪⸙Zifabotz-MD⳹ ❋ཻུ۪۪' //Main Watermark
-global.wm2 = '⳹ ❋ཻུ۪۪⸙Zifabotz-MD⳹ ❋ཻུ۪۪'
-global.wm3 = '⳹ ❋ཻུ۪۪⸙Zifabotz-MD⳹ ❋ཻུ۪۪'
+global.wm = 'Radityaa-MD' //Main Watermark
+global.wm2 = 'Radityaa-MD'
+global.wm3 = 'Radityaa-MD'
 global.botdate = `☘︎ 𝗗𝗮𝘁𝗲: ${week} ${date}\n☘︎ 𝗧𝗶𝗺𝗲: ${wktuwib}`
 global.bottime = `𝗧 𝗜 𝗠 𝗘 : ${wktuwib}`
 global.titlebot = `☘︎ 𝗪𝗵𝗮𝘁𝘀𝗮𝗽𝗽 𝗕𝗼𝘁 𝗠𝘂𝗹𝘁𝗶-𝗗𝗲𝘃𝗶𝗰𝗲| By ${global.nameown}`
-global.author = '┄┄┄┅┅❑ ROZI ❑┅┅┄┄┄'
+global.author = '┄┄┄┅┅❑ RadityaaX ❑┅┅┄┄┄'
 
 
 /*============== LOGO ==============*/
